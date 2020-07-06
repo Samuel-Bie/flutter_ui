d025b6da22e6f6361dd45daa124e820ed79eeb26
samuel-bie <samuelbiezinho@gmail.com>
2020-07-06 15:51:08 +0200

    A basic fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-// import 'package:ui_project/views/home/home_view.dart';
+import 'package:ui_project/views/home/home_view.dart';
 import 'package:ui_project/views/login/login_view.dart';
 
 void main() {
-  runApp(LoginView());
+  runApp(MyApp());
 }
